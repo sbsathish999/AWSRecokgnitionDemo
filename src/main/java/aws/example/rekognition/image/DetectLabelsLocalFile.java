0ea86a7b5e3d95fb68d681cb5e762125a8c81c5a
Sathish Babu Padmanabhan <s.babu@prisma-it.com>
2022-11-15 09:14:56 +0100

    first commit along with detect lables from the image functionalities

---
+++ src/main/java/aws/example/rekognition/image/DetectLabelsLocalFile.java
@@ -0,0 +1,81 @@
+package aws.example.rekognition.image;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.rekognition.RekognitionClient;
+import software.amazon.awssdk.services.rekognition.model.Image;
+import software.amazon.awssdk.services.rekognition.model.DetectLabelsRequest;
+import software.amazon.awssdk.services.rekognition.model.DetectLabelsResponse;
+import software.amazon.awssdk.services.rekognition.model.Label;
+import software.amazon.awssdk.services.rekognition.model.RekognitionException;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+
+public class DetectLabelsLocalFile {
+    public static void main(String[] args) {
+
+//        final String usage = "\n" +
+//                "Usage: " +
+//                "   <sourceImage>\n\n" +
+//                "Where:\n" +
+//                "   sourceImage - The path to the image (for example, C:\\AWS\\pic1.png). \n\n";
+//
+//        if (args.length != 1) {
+//            System.out.println(usage);
+//            System.exit(1);
+//        }
+
+        try {
+            String sourceImage = "unknown2.jpg";
+            Region region = Region.US_EAST_1;
+            RekognitionClient rekClient = RekognitionClient.builder()
+                    .region(region)
+                    .credentialsProvider(ProfileCredentialsProvider.create())
+                    .build();
+
+            detectImageLabels(rekClient, sourceImage);
+            rekClient.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // snippet-start:[rekognition.java2.detect_labels.main]
+    public static void detectImageLabels(RekognitionClient rekClient, String sourceImage) {
+
+        try {
+            InputStream sourceStream = new FileInputStream(sourceImage);
+            SdkBytes sourceBytes = SdkBytes.fromInputStream(sourceStream);
+
+            // Create an Image object for the source image.
+            Image souImage = Image.builder()
+                    .bytes(sourceBytes)
+                    .build();
+
+            DetectLabelsRequest detectLabelsRequest = DetectLabelsRequest.builder()
+                    .image(souImage)
+                    .maxLabels(30)
+                    .build();
+
+            DetectLabelsResponse labelsResponse = rekClient.detectLabels(detectLabelsRequest);
+            List<Label> labels = labelsResponse.labels();
+            System.out.println("Detected labels for the given photo");
+            int i = 1;
+            for (Label label: labels) {
+                System.out.println( i + " - " + label.name() + ": " + label.confidence().toString());
+                i++;
+            }
+
+        } catch (RekognitionException | FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/aws/example/rekognition/image/DetectLabelsS3.java
@@ -0,0 +1,61 @@
+package aws.example.rekognition.image;
+
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.rekognition.RekognitionClient;
+import software.amazon.awssdk.services.rekognition.model.Image;
+import software.amazon.awssdk.services.rekognition.model.DetectLabelsRequest;
+import software.amazon.awssdk.services.rekognition.model.DetectLabelsResponse;
+import software.amazon.awssdk.services.rekognition.model.Label;
+import software.amazon.awssdk.services.rekognition.model.RekognitionException;
+import software.amazon.awssdk.services.rekognition.model.S3Object;
+import java.util.List;
+public class DetectLabelsS3 {
+
+    public static void main(String[] args) {
+
+        String bucket = "1-bucket-rekognition-demo-1";
+        String image = "unknown2.jpg";
+        Region region = Region.US_EAST_1;
+        RekognitionClient rekClient = RekognitionClient.builder()
+                .region(region)
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .build();
+
+        getLabelsfromImage(rekClient, bucket, image);
+        rekClient.close();
+    }
+
+    // snippet-start:[rekognition.java2.detect_labels_s3.main]
+    public static void getLabelsfromImage(RekognitionClient rekClient, String bucket, String image) {
+
+        try {
+            S3Object s3Object = S3Object.builder()
+                    .bucket(bucket)
+                    .name(image)
+                    .build();
+
+            Image myImage = Image.builder()
+                    .s3Object(s3Object)
+                    .build();
+
+            DetectLabelsRequest detectLabelsRequest = DetectLabelsRequest.builder()
+                    .image(myImage)
+                    .maxLabels(25)
+                    .build();
+
+            DetectLabelsResponse labelsResponse = rekClient.detectLabels(detectLabelsRequest);
+            List<Label> labels = labelsResponse.labels();
+            System.out.println("Detected labels for the given photo");
+            int i=1;
+            for (Label label : labels) {
+                System.out.println(i + " - " + label.name() + ": " + label.confidence().toString() + label.parents());
+                i++;
+            }
+
+        } catch (RekognitionException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+}
